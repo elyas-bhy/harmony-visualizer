8e25bd0647c645dcb0594ef7e01e09f55da3b048
elyas-bhy <elyas.bhy@gmail.com>
2013-12-07 16:33:34 +0100

    Added more test data.

--- js/mapping.js
+++ js/mapping.js
@@ -1,10 +1,20 @@
 var mapping={
 	"D1":"alpha",
-	"D2":"beta",
-	"D3":"gamma",
+	"D2":"bravo",
+	"D3":"charlie",
 	"D4":"delta",
+	"D5":"echo",
+	"D6":"foxtrot",
+	"D7":"golf",
+	"D8":"hotel",
+	"D9":"india",
 
-	"M1":"com.app.network",
-	"M2":"com.app.test",
-	"M3":"com.app.util"
+	"M1":"com.app.generic",
+	"M2":"com.app.io",
+	"M3":"com.app.lib",
+	"M4":"com.app.model",
+	"M5":"com.app.stats",
+	"M6":"com.app.test",
+	"M7":"com.app.trace",
+	"M8":"com.app.util"
 }
