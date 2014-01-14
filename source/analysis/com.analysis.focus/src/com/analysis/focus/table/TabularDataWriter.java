40331e39d4f1afafa3139dc5a29ed7a296e75958
elyas-bhy <elyas.bhy@gmail.com>
2014-01-14 17:14:21 +0100

    Added column for total contributions in tabular view

--- source/analysis/com.analysis.focus/src/com/analysis/focus/table/TabularDataWriter.java
+++ source/analysis/com.analysis.focus/src/com/analysis/focus/table/TabularDataWriter.java
@@ -6,34 +6,50 @@ import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
 
+import com.analysis.focus.Component;
 import com.analysis.focus.Contributor;
 import com.analysis.focus.Distribution;
 
 public class TabularDataWriter {
-	
+
 	private Map<String,Contributor> contributors;
+	private Map<String,Component> components;
 	private StringBuffer data;
 	
-	public TabularDataWriter(Map<String,Contributor> contributors) {
+	public TabularDataWriter(Map<String,Contributor> contributors, Map<String,Component> components) {
 		this.contributors = contributors;
+		this.components = components;
 		this.data = new StringBuffer();
 	}
 
 	public void generateTable() {
+		int total = 0;
 		data.append("[");
 		for (Contributor contributor : contributors.values()) {
 			data.append("{ \"name\":\"");
 			data.append(contributor.getName());
-			data.append("\" ");
+			data.append("\"");
 			for (Distribution d : contributor.getContributionMap().values()) {
 				data.append(",\"" + d.getName());
 				data.append("\":\"");
 				data.append(d.getContributions());
-				data.append("\" ");
+				data.append("\"");
+				total += d.getContributions();
 			}
-			data.append("},");
+			data.append(",\"total\":\"");
+			data.append(total);
+			data.append("\"},");
+			total = 0;
 		}
-		data.append("]");
+		
+		data.append("{ \"name\":\" TOTAL\"");
+		for (Component component : components.values()) {
+			data.append(",\"" + component.getName());
+			data.append("\":\"");
+			data.append(component.getContributions());
+			data.append("\"");
+		}
+		data.append("}]");
 		
 		File file = new File("tview.json");
 		try {
